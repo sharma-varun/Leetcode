955ff3865a3fe3b064912dc95553ad52521eeb2f
Varun Sharma <mvasharm@amazon.com>
2025-12-14 20:49:59 -0800

    sorted squares

--- Array/sortedSquares.js
+++ Array/sortedSquares.js
@@ -3,8 +3,26 @@
  * @return {number[]}
  */
 const sortedSquares = function (nums) {
-  const squaredArray = nums.map((num) => num * num);
+  const result = Array(nums.length).fill(0);
 
-  // You can use any popular sorting algorithm i.e. Merge sort, Quick sort, etc
-  return squaredArray.sort((a, b) => a - b);
+  let i = 0;
+  let j = nums.length - 1;
+  let k = j;
+
+  while (j >= i) {
+    const val1 = Math.abs(nums[i]);
+    const val2 = Math.abs(nums[j]);
+
+    if (val1 > val2) {
+      result[k] = val1 * val1;
+      i++;
+      k--;
+    } else {
+      result[k] = val2 * val2;
+      j--;
+      k--;
+    }
+  }
+
+  return result;
 };
